4e83822ea10bcb07cd11e97a3b7c03de0eb48837
Feralitt <hdjdjsjxnxjdjejejddk@gmail.com>
2024-06-29 12:20:07 +0300

    fix button weight in game

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pmc_shishki.colors"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.5.2"
+        versionCode = 12
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
